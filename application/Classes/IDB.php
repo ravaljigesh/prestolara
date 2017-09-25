75b5d3b27641394995e0d460ec450880c72dd76a
Jigesh Raval <jigeshraval1989@gmail.com>
2017-09-25 15:04:15 +0530

    Second commit

--- application/Classes/IDB.php
+++ application/Classes/IDB.php
@@ -19,16 +19,20 @@ class IDB extends Capsule
 
     public function connect()
     {
+        $ps = include(_PS_ROOT_DIR_ . '/app/config/parameters.php');
+        if (isset($ps['parameters'])) {
+          $ps = $ps['parameters'];
+        }
         $capsule = new IDB;
         $capsule->addConnection([
           'driver' => 'mysql',
-          'host' => '127.0.0.1',
-          'database' => 'mymusic_dev',
-          'username' => 'mymusic_jigesh',
-          'password' => 'Ravalera1#',
+          'host' => $ps['database_host'],
+          'database' => $ps['database_name'],
+          'username' => $ps['database_user'],
+          'password' => $ps['database_password'],
           'charset' => 'utf8',
           'collation' => 'utf8_unicode_ci',
-          'prefix' => 'ms_'
+          'prefix' => $ps['database_prefix']
         ]);
 
         $capsule->setAsGlobal();
